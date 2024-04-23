4578d0dc48b2c872043bc9020f0e153b255e6c26
meme-h <makehope1015@gmail.com>
2024-04-23 22:47:05 +0900

    assignment project publishing

--- index.js
+++ index.js
@@ -3,3 +3,15 @@
     사용해야할 경우가 있을 경우 사용해주세요.
     javascript 라이브러리 사용은 지양하지만 jquery는 사용하셔도 무방합니다.
 */
+$(document).ready(function(){
+    // tab menu
+    $('.tab-menu li').on('click', function() {
+        const num = $('.tab-menu li').index($(this));
+        
+        $('.tab-menu li').removeClass('active');
+        $('.tab-con > div').removeClass('on');
+
+        $('.tab-menu li:eq(' + num + ')').addClass('active');
+        $('.tab-con > div:eq(' + num + ')').addClass('on');
+    });
+})
